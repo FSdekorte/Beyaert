7ce37f859e0adc716e089494e3afa02afca74ffe
FSdekorte <solomon.francois@hotmail.com>
2024-05-24 18:13:49 +0200

    error styling

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,12 +1,27 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
+import { AlertCircle } from "lucide-react"
 
-export default function ErrorPage() {
- return           <Alert>
- <Terminal className="h-4 w-4" />
- <AlertTitle>Komt dadelijk</AlertTitle>
- <AlertDescription>
-   <h1 className="text-5xl font-medium text-white">404</h1>
- </AlertDescription>
-</Alert>
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import { BreadcrumbLink } from "@/components/ui/breadcrumb"
+import Link from "next/link"
+
+export default function AlertDestructive() {
+  return (
+    <section className="h-screen w-screen flex items-center justify-center">
+    <Alert variant="destructive" className="w-fit text-red-700">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle >Error</AlertTitle>
+      <AlertDescription className="text-green">
+        Iets ging fout, Probeer opnieuw.
+      </AlertDescription>
+    </Alert>
+  
+    <BreadcrumbLink href="/" className="transition-all duration-300 absolute text-3xl text-red-700 hover:text-green mt-[6em]">
+        Home
+      </BreadcrumbLink>
+    </section>
+  )
 }
